afce21e58c6e0d69221b27c23f88192b573d1802
阅卷 <32693484+littleguyy@users.noreply.github.com>
2018-02-05 03:34:20 +0800

    Add files via upload

--- pygame测试.py
+++ pygame测试.py
@@ -2,9 +2,25 @@
 import pygame
 
 pygame.init()
-游戏分辨率=pygame.display.set_mode((800,600))
-pygame.display.set_caption('A bit Racey')
+
+窗口高=600
+窗口宽=800
+
+黑=(0,0,0)
+白=(255,255,255)
+红=(255,0,0)
+
+游戏分辨率=pygame.display.set_mode((窗口宽,窗口高))
+pygame.display.set_caption('游戏测试')
 时钟=pygame.time.Clock()
+
+图=pygame.image.load('i:/images.jpg')
+
+def 图位置(x,y):
+    游戏分辨率.blit(图,(x,y))
+
+x=(窗口宽*0.45)
+y=(窗口高*0.5)
 
 崩=False
 
@@ -13,9 +29,16 @@ while not 崩:
     for 事件 in pygame.event.get():
         if 事件.type == pygame.QUIT:
             崩=True
-
-        print(事件)
+        # print(事件)
+        
+        
+    游戏分辨率.fill(白)
+    if 事件.type == pygame.MOUSEMOTION:
+        x,y=事件.pos
+    图位置(x,y)
+    
     pygame.display.update()
     时钟.tick(60)
+    
 pygame.quit()
 quit()
